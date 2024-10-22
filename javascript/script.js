9f1195290f82465ed6af49a8c7477b6711aa4741
Carlos Teixeira <c.texeirajr@gmail.com>
2024-10-22 16:15:27 -0300

    versão 1.4 (vetor)
    desenvolvimento de javascript captando valores dinamicos dentro de um array

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,16 @@
 let nome = [];
-function pegaNome(){ 
+let listaPlaceHolders = ["DIGITE SEU SEGUNDO NOME AQUI","FUNCIONOU"];
+let c = 0;
+
+function execucao(){ 
     let inputPrincipal = document.getElementById('inputPrincipal');
     nome.push(inputPrincipal.value);
-    
+    inputPrincipal.value = '';
+    inputPrincipal.placeholder = listaPlaceHolders[c];
+    window.alert(`${nome}`);
+    window.alert(`Vetor com ${nome.length} posições`)
+    c++;
 }
+
+
 
