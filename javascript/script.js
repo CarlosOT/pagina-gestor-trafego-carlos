d8e024012e91b1c87810f953790120d23012a0cc
Carlos Teixeira <c.texeirajr@gmail.com>
2024-10-23 19:19:51 -0300

    satisfatório

--- javascript/script.js
+++ javascript/script.js
@@ -3,21 +3,34 @@ let nome = [];
 let listaPlaceHolders = ["JÁ TEM CONTA NO GOOGLE ADS?","INFORME SEU SITE","INFORME SEU PRINCIPAL SERVIÇO"];
 let c = 0;
 
-//PARA ESCONDER ÁREA DE BOTÃO
-let idWhatsapp = document.getElementById('whatsapp');
-idWhatsapp.style.display = "none";
+//ESCONDER BOTÃO DE WHATSAPP
+let botaoWhatsapp = document.querySelector('.wrapper');
+botaoWhatsapp.style.display = "none";
+
+//TEMPORÁRIO PARA TESTES
+inputPrincipal.style.display = "";
+botaoOk.style.display = "";
+
 
 function execucao(){ 
     let inputPrincipal = document.getElementById('inputPrincipal');
+    let botaoOk = document.getElementById('botaoOk');
+    
+
     if(inputPrincipal.value.length == 0){
         window.alert('O CAMPO NÃO PODE FICAR VAZIO')
     }else{
         nome.push(inputPrincipal.value);
         inputPrincipal.value = '';
         inputPrincipal.placeholder = listaPlaceHolders[c];
-        window.alert(`${nome}`);
-        window.alert(`Vetor com ${nome.length} posições`)
         c++;
+        
+        if(nome.length == 4){
+            inputPrincipal.style.display = "none";
+            botaoOk.style.display = "none";
+            botaoWhatsapp.style.display = "";
+        }
+
     }
 }
 
