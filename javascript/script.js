fb0da10728507f977cf81d8cfbb6e782b3ef6933
Carlos Teixeira <c.texeirajr@gmail.com>
2024-10-23 18:23:08 -0300

    versão 1.13 (lógica funcionando)

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,6 @@
 //VETOR
 let nome = [];
-let listaPlaceHolders = ["DIGITE SEU SEGUNDO NOME AQUI","FUNCIONOU"];
+let listaPlaceHolders = ["JÁ TEM CONTA NO GOOGLE ADS?","INFORME SEU SITE","INFORME SEU PRINCIPAL SERVIÇO"];
 let c = 0;
 
 //PARA ESCONDER ÁREA DE BOTÃO
@@ -10,24 +10,15 @@ idWhatsapp.style.display = "none";
 
 function execucao(){ 
     let inputPrincipal = document.getElementById('inputPrincipal');
-    nome.push(inputPrincipal.value);
-    inputPrincipal.value = '';
-    inputPrincipal.placeholder = listaPlaceHolders[c];
-    window.alert(`${nome}`);
-    window.alert(`Vetor com ${nome.length} posições`)
-    c++;
+    if(inputPrincipal.value.length == 0){
+        window.alert('O CAMPO NÃO PODE FICAR VAZIO')
+    }else{
+        nome.push(inputPrincipal.value);
+        inputPrincipal.value = '';
+        inputPrincipal.placeholder = listaPlaceHolders[c];
+        window.alert(`${nome}`);
+        window.alert(`Vetor com ${nome.length} posições`)
+        c++;
+    }
 }
-
-//QUAIS SÃO AS INFORMAÇÕES ESSENCIAIS QUE PRECISO DE UM CLIENTE QUE ESTÁ PROCURANDO POR UM GESTOR DE TRÁFEGO?
-/*
-NOME
-PALAVRA CHAVE PRINCIPAL (SERVIÇO)
-AREA
-TEM CONTA NO GOOGLE ADS?
-TEM CAMPANHAS ATIVAS?
-JÁ TRABALHOU COM ALGUM GESTOR DE TRÁFEGO?
-
-
-Oi Carlos, meu nome é ${NOME}. Eu sou da area de ${AREA}, desejo contratar um gestor de tráfego para vender no google meu principal serviço, que é ${SERVIÇO}.
-*/
 
