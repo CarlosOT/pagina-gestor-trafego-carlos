dee6969472e32825d828e4aad072c767e481b686
Carlos Teixeira <c.texeirajr@gmail.com>
2024-10-28 17:15:44 -0300

    v2.0
    +colocar aviso no window.alert ( ) para quando clicar no botão do whatsapp
    
    +desativado (por comentários) o botão para enviar para o whatsapp desktop
    
    +desativado titulo principal que aparece no final
    
    +ajuste de copy

--- javascript/script.js
+++ javascript/script.js
@@ -29,9 +29,10 @@ let largura = 0;
 
 //LINKS WHATSAPP
 let whatsWeb = document.getElementById('linkWhatsWeb');
-let whatsDesk = document.getElementById('linkWhatsDesk');
-let linkWhatsDesk = "https://api.whatsapp.com/send?phone=5511968255910&text=Ol%C3%A1%20Carlos,%20tudo%20bem?%20Estou%20precisando%20contratar%20um%20gestor%20de%20tr%C3%A1fego%20para%20a%20minha%20empresa,%20segue%20abaixo%20os%20dados%20que%20preenchi%20no%20site,%20aguardo%20seu%20retorno!%0A%0A";
 let linkWhatsWeb = "https://web.whatsapp.com/send?phone=5511968255910&text=Ol%C3%A1%20Carlos,%20tudo%20bem?%20Estou%20precisando%20contratar%20um%20gestor%20de%20tr%C3%A1fego%20para%20a%20minha%20empresa,%20segue%20abaixo%20os%20dados%20que%20preenchi%20no%20site,%20aguardo%20seu%20retorno!%0A%0A";
+// let whatsDesk = document.getElementById('linkWhatsDesk');
+// let linkWhatsDesk = "https://api.whatsapp.com/send?phone=5511968255910&text=Ol%C3%A1%20Carlos,%20tudo%20bem?%20Estou%20precisando%20contratar%20um%20gestor%20de%20tr%C3%A1fego%20para%20a%20minha%20empresa,%20segue%20abaixo%20os%20dados%20que%20preenchi%20no%20site,%20aguardo%20seu%20retorno!%0A%0A";
+
 
 inputPrincipal.focus();
 function execucao(){ 
@@ -47,7 +48,7 @@ function execucao(){
         nome.push(String(inputPrincipal.value).toUpperCase());
         inputPrincipal.value = '';
         inputPrincipal.placeholder = listaPlaceHolders[c];
-        linkWhatsDesk += `${listaRespostas[c]}%20*${nome[c]}*%20%0A`;
+        //linkWhatsDesk += `${listaRespostas[c]}%20*${nome[c]}*%20%0A`;
         linkWhatsWeb += `${listaRespostas[c]}%20*${nome[c]}*%20%0A`;
         c++;
         progresso += 25;
@@ -62,13 +63,14 @@ function execucao(){
             tituloH2_2.style.display = "";
 
             headLine.style.display = "";
-            headLine.innerHTML = "Para nos enviar as informações preenchidas, ABRA seu Whatsapp no computador e CLIQUE em uma das opções abaixo para acessar pelo:";
+            headLine.innerHTML = "Clique no botão abaixo para falar conosco. Tire todas as suas dúvidas e contrate assessoria completa em gestão de tráfego google ads para a sua empresa por um preço especial que cabe no seu bolso!";
 
             inputPrincipal.style.display = "none";
             botaoOk.style.display = "none";
             botaoWhatsapp.style.display = "";
-            whatsDesk.href = linkWhatsDesk;
+            //whatsDesk.href = linkWhatsDesk;
             whatsWeb.href = linkWhatsWeb;
+            barraProgresso.style.display = "none";
             
             borda.style.borderColor = "rgb(66, 241, 2)";
             borda.style["boxShadow"] = "1px 1px 5px #42f102";
@@ -76,7 +78,7 @@ function execucao(){
             imgCarlos.style.borderColor = "rgb(66, 241, 2)";
             imgCarlos.style["boxShadow"] = "1px 1px 8px #42f102";
 
-            barraProgresso.style.backgroundColor = "#42f102";
+            //barraProgresso.style.backgroundColor = "#42f102";
 
         }
 
@@ -84,3 +86,12 @@ function execucao(){
     inputPrincipal.focus();
 }
 
+
+// function clickWhatsWeb(){
+//     window.alert(`[ATENÇÃO ${nome[0]}] Você está sendo redirecionado para a página oficial do WHATSAPP WEB. Caso seu Whatsapp não esteja aberto na web, basta escanear o QR Code através do app do seu whatsapp`);
+
+// }
+
+// function clickWhatsDesk(){
+// window.alert(`[ATENÇÃO ${nome[0]}] Você escolheu acessar o Whatsapp pelo Desktop. Para que as informações preenchidas sejam enviadas automaticamente, basta que ao abrir seu whatsapp desktop, você minimize a tela, volte para a tela do whatsapp no computador e clique para "iniciar a conversa" novamente, dessa forma todos os dados serão enviados como parâmetros junto com a sua mensagem.`);
+// }
